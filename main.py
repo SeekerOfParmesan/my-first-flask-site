14267e6ce060dde2039c4af096bbebfb5ec9ca2b
rik blokzijl <86717462+SeekerOfParmesan@users.noreply.github.com>
2021-11-01 08:52:13 +0000

    >Added all guns to Guns.html

--- main.py
+++ main.py
@@ -48,20 +48,43 @@ all = [
 
 ]
 
-
-# legends = ['Bloodhound','Gibraltar','Lifeline','Pathfinder','Wraith','Bangalore','Caustic','Mirage','Octane','Wattson','Crypto','Revenant','Loba','Rampart','Horizon','Fuse','Valkyrie','Seer']
-# seasons = ['Season 0','Season 0','Season 0','Season 0','Season 0','Season 0','Season 0','Season 0','Season 1','Season 2','Season 3','Season 4','Season 5','Season 6','Season 7','Season 8','Season 9','Season 10']
-
-
 @app.route('/info/legends/')
 def leg_ends():
   return render_template('legends.html', all=all)
 
 #guns
-gun_go_brr = ['R301','P2020']
+all = [
+  ['R-301 carbine', 'Light ammo', 'R201'],
+  ['P2020', 'Light ammo', 'P2019'],
+  ['RE-45 auto', 'Light ammo', 'RE-45'],
+  ['R-99', 'Light ammo', 'R-97']
+  ['G7 scout', 'Light ammo', 'G7 scout']
+  ['Alternator', 'Carepackage(Light ammo)', 'Alternator']
+  ['VK-47 flatline', 'Heavy ammo', 'VK-47 flatline']
+  ['Rampage', 'Heavy ammo', 'N.A']
+  ['Prowler burst PDW', 'Heavy ammo', 'N.A']
+  ['Hemlok burst AR', 'Heavy ammo', 'Hemlok burst AR']
+  ['Wingman', 'Heavy ammo', 'Wingman']
+  ['30-30 Repeater', 'Heavy ammo', 'N.A']
+  ['M600 Spitfire', 'Carepackage(Heavy ammo)', 'M600 Spitfire']
+  ['Bocek compound bow', 'Arrows', 'N.A']
+  ['HAVOC Rifle', 'Energy ammo', 'N.A']
+  ['Devotion LMG', 'Energy ammo', 'Devotion LMG']
+  ['Volt SMG', 'Energy ammo', 'Volt SMG']
+  ['L-STAR EMG', 'Energy ammo', 'L-STAR EMG']
+  ['Triple Take', 'Carepackage(Energy ammo)', 'Double Take']
+  ['EVA-8 Auto', 'Shotgun ammo', 'EVA-8 Auto']
+  ['Mastiff Shotgun', 'Shotgun ammo', 'Mastiff Shotgun']
+  ['Mozambique Shotgun', 'Shotgun ammo', 'Mozambique Shotgun']
+  ['Peacekeaper', 'Shotgun ammo', 'N.A']
+  ['Charge Rifle', 'Sniper ammo', 'Charge Rifle']
+  ['Longbow DMR', 'Sniper ammo', 'DMR']
+  ['Sentinel', 'Sniper ammo', 'N.A']
+  ['Kraber .50 cal', 'Carepackage(Sniper)', 'Kraber']
+]
 @app.route('/info/guns/')
 def guns():
-  return render_template('guns.html',gun_go_brr=gun_go_brr)
+  return render_template('guns.html', all=all)
 
 main_legend = []
 
@@ -73,17 +96,6 @@ def mains():
 
   return render_template('legend_main.html',
   main_legend = main_legend)
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
